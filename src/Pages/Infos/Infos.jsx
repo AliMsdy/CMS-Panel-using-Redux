e4e475d87d0e8d6a45a931afc34fbb403b66b2d2
Ali <alimoosabadi326@gmail.com>
2023-09-05 23:44:33 +0330

    edit the infos route

--- src/Pages/Infos/Infos.jsx
+++ src/Pages/Infos/Infos.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Infos() {
-  return <div>Infos</div>;
+  return (
+    <div>
+      Infos
+      <p className="mt-5 rounded-full bg-red-500 text-white">
+        some extra information
+      </p>
+    </div>
+  );
 }
 
 export default Infos;
