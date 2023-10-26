5c7d1b49faa9661153904ad5b993233568b57b3a
Ali <alimoosabadi326@gmail.com>
2023-10-26 13:57:45 +0330

    adding accordion to Infos page

---
+++ src/Components/UI/Accordion/Accordion.jsx
@@ -0,0 +1,44 @@
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import { styled } from "@mui/material/styles";
+
+const Accordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={3} {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&:before": {
+    display: "none",
+  },
+}));
+
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === "dark"
+      ? theme.palette.background.secondary
+      : "rgba(33, 37, 41, 0.03)",
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+  "& .MuiAccordionSummary-content": {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+  backgroundColor: theme.palette.background.secondary
+}));
+
+export { Accordion, AccordionDetails, AccordionSummary };
--- src/Pages/Infos/Infos.jsx
+++ src/Pages/Infos/Infos.jsx
@@ -1,14 +1,20 @@
 //components
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+} from "../../Components/UI/Accordion/Accordion";
 import { InfoInput, PasswordInput } from "../../Components/UI/Input/Input";
-import UploaderBox from "../../Components/UploadBox/UploadBox";
 import MuiButton from "../../Components/UI/MuiButton/MuiButton";
+import UploaderBox from "../../Components/UploadBox/UploadBox";
 
 //icons
-import PersonIcon from "@mui/icons-material/Person";
 import GroupIcon from "@mui/icons-material/Group";
-import PermContactCalendarIcon from "@mui/icons-material/PermContactCalendar";
-import LanguageIcon from "@mui/icons-material/Language";
 import KeyIcon from "@mui/icons-material/Key";
+import LanguageIcon from "@mui/icons-material/Language";
+import PermContactCalendarIcon from "@mui/icons-material/PermContactCalendar";
+import PersonIcon from "@mui/icons-material/Person";
+import { useState } from "react";
 
 const InputArray = [
   [
@@ -27,59 +33,77 @@ const InputArray = [
 ];
 
 function Infos() {
+  const [expanded, setExpanded] = useState("panel1");
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
   return (
     <div className="px-6 py-8">
-      <div className="border-1 rounded-t-lg border border-[#0000002d]">
-        {/* user information */}
-        <div className="bg-[#21252908] p-4">
-          <a className="font-bold" href="#form">
-            اطلاعات شما
-          </a>
-        </div>
-        {/* form Inputs */}
-        <div className="border-t-2 border-t-[#0000002d] px-4 py-3">
-          <form className="flex flex-col gap-y-4" id="form">
-            {InputArray.map((inputArray, index) => {
-              return (
-                <div
-                  className="flex flex-col flex-wrap gap-4 sm:flex-row sm:justify-evenly md:flex-nowrap"
-                  key={index}
-                >
-                  {inputArray.map((input) => {
-                    if (input.type === "password")
-                      return <PasswordInput {...input} key={input.label} />;
-                    return <InfoInput {...input} key={input.label} />;
-                  })}
-                </div>
-              );
-            })}
-          </form>
-        </div>
-        {/* uploader section */}
-        <div className="mt-8 flex flex-col gap-6 p-4 md:flex-row">
-          <UploaderBox
-            imgSrc="/img/admin/profile/banana.png"
-            title="پروفایل"
-            isBanner={false}
-          />
-          <UploaderBox
-            imgSrc="/img/admin/banner/banner.png"
-            title="بنر"
-            isBanner={true}
-          />
-        </div>
-        {/* update information Button */}
-        <div className="border-b-2 border-b-[#0000002d]  p-4 text-center">
-          <MuiButton customcolor="#009cf0" size="large">
-            آپدیت اطلاعات
-          </MuiButton>
-        </div>
-        {/* team information */}
-        <div className="bg-[#21252908] p-4">
-          <a className="font-bold" href="#team">
-            اطلاعات تیم
-          </a>
-        </div>
+      <div className="border-1 rounded-t-lg border ">
+        <Accordion
+          expanded={expanded === "panel1"}
+          onChange={handleChange("panel1")}
+        >
+          {/* user information */}
+          <AccordionSummary>
+            <div className=" p-4">
+              <a className="font-bold">اطلاعات شما</a>
+            </div>
+          </AccordionSummary>
+          <AccordionDetails>
+            {/* form Inputs */}
+            <div className="px-4 py-3">
+              <form className="flex flex-col gap-y-4">
+                {InputArray.map((inputArray, index) => {
+                  return (
+                    <div
+                      className="flex flex-col flex-wrap gap-4 sm:flex-row sm:justify-evenly md:flex-nowrap"
+                      key={index}
+                    >
+                      {inputArray.map((input) => {
+                        if (input.type === "password")
+                          return <PasswordInput {...input} key={input.label} />;
+                        return <InfoInput {...input} key={input.label} />;
+                      })}
+                    </div>
+                  );
+                })}
+              </form>
+            </div>
+            {/* uploader section */}
+            <div className="mt-8 flex flex-col gap-6 p-4 md:flex-row">
+              <UploaderBox
+                imgSrc="/img/admin/profile/banana.png"
+                title="پروفایل"
+                isBanner={false}
+              />
+              <UploaderBox
+                imgSrc="/img/admin/banner/banner.png"
+                title="بنر"
+                isBanner={true}
+              />
+            </div>
+            {/* update information Button */}
+            <div className=" p-4 text-center">
+              <MuiButton customcolor="#009cf0" size="large">
+                آپدیت اطلاعات
+              </MuiButton>
+            </div>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion
+          expanded={expanded === "panel2"}
+          onChange={handleChange("panel2")}
+        >
+          {/* team information */}
+          <AccordionSummary>
+            <div className=" p-4">
+              <a className="font-bold">اطلاعات تیم</a>
+            </div>
+          </AccordionSummary>
+        </Accordion>
       </div>
     </div>
   );
